624a122bad1e6c98f44c588044e7142fb21c2801
Ruslan-Lukyanets <ruslanlukyanets@yandex.ru>
2026-03-02 17:54:53 +0300

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -2,8 +2,6 @@ import pytest
 
 from pathlib import Path
 
-from playwright.sync_api import Page, expect
-
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
 
@@ -37,9 +35,8 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     )
     create_course_page.click_create_course_button()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.course_view.check_visible(
         index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
     )
 
@@ -52,6 +49,5 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.navbar.check_visible('username')
     courses_list_page.sidebar.check_visible()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
