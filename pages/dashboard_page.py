594ec98a8987800d45fe6fc91083e8a75ad88f68
Ruslan-Lukyanets <ruslanlukyanets@yandex.ru>
2026-03-02 16:36:08 +0300

    Знакомство с PageComponent

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page, expect
 
 from components.navigation.navbar_component import NavbarComponent
+from components.navigation.sidebar_component import SidebarComponent
 from pages.base_page import BasePage
 
 
@@ -9,6 +10,7 @@ class DashboardPage(BasePage):
         super().__init__(page)
 
         self.navbar = NavbarComponent(page)
+        self.sidebar = SidebarComponent(page)
 
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
@@ -23,7 +25,6 @@ class DashboardPage(BasePage):
 
         self.scores_title = page.get_by_test_id('scores-widget-title-text')
         self.scores_chart = page.get_by_test_id('scores-scatter-chart')
-
 
     def check_visible_dashboard_title(self):
         expect(self.dashboard_title).to_be_visible()
