987f52948a795b46e3264e8c3fb130107c1b2f3d
Ruslan-Lukyanets <ruslanlukyanets@yandex.ru>
2026-03-03 18:06:56 +0300

    Наращивание тестовой базы и рефакторинг

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,5 +1,9 @@
 import pytest
+import os
 from playwright.sync_api import Page, Playwright
+
+from pages.authentication.registration_page import RegistrationPage
+
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
@@ -13,26 +17,24 @@ def initialize_browser_state(playwright: Playwright):
         context = browser.new_context()
         page = context.new_page()
 
+        registration_page = RegistrationPage(page=page)
+        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.registration_form.fill(email='user@gmail.com', username='username', password='password')
         page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.click_registration_button()
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user@gmail.com')
-
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        context.storage_state(path="browser-state.json")
+        parent_dir = os.path.dirname(os.path.abspath(__file__))
+        root_dir = os.path.dirname(parent_dir)
+        storage_state_path = os.path.join(root_dir, "browser-state.json")
+        context.storage_state(path=storage_state_path)
         browser.close()
 
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright):
         browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json")
+        parent_dir = os.path.dirname(os.path.abspath(__file__))
+        root_dir = os.path.dirname(parent_dir)
+        storage_state_path = os.path.join(root_dir, "browser-state.json")
+        context = browser.new_context(storage_state=storage_state_path)
         yield context.new_page()
         browser.close()
--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,8 +1,8 @@
 import pytest
 from pathlib import Path
 
-from pages.courses_list_page import CoursesListPage
-from pages.create_course_page import CreateCoursePage
+from pages.courses.courses_list_page import CoursesListPage
+from pages.courses.create_course_page import CreateCoursePage
 
 
 @pytest.mark.courses
@@ -19,8 +19,9 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     create_course_page.create_course_exercises_toolbar_view.check_visible()
     create_course_page.check_visible_exercises_empty_view()
 
-    file_path = str(Path(__file__).resolve().parent.parent / "testdata" / "files" / "image.png")
+    file_path = str(Path(__file__).resolve().parent.parent.parent / "testdata" / "files" / "image.png")
     create_course_page.image_upload_widget.upload_preview_image(file_path)
+    #create_course_page.image_upload_widget.upload_preview_image("/testdata/files/image.png")
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.create_course_form.fill(
         title="Playwright",
