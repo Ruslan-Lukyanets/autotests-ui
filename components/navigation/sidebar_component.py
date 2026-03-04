6018e4188926da6d719a9aa5dc1cbe1cc41c4eda
Ruslan-Lukyanets <ruslanlukyanets@yandex.ru>
2026-03-04 16:45:37 +0300

    Наращивание тестовой базы и рефакторинг

--- components/navigation/sidebar_component.py
+++ components/navigation/sidebar_component.py
@@ -20,10 +20,10 @@ class SidebarComponent(BaseComponent):
         self.dashboard_list_item.check_visible('Dashboard')
 
     def click_logout(self):
-        self.logout_list_item.navigate_url(re.compile(r".*/#/auth/login"))
+        self.logout_list_item.navigate(re.compile(r".*/#/auth/login"))
 
     def click_courses(self):
-        self.courses_list_item.navigate_url((re.compile(r".*/#/courses")))
+        self.courses_list_item.navigate((re.compile(r".*/#/courses")))
 
     def click_dashboard(self):
-        self.dashboard_list_item.navigate_url(re.compile(r".*/#/dashboard"))
+        self.dashboard_list_item.navigate(re.compile(r".*/#/dashboard"))
