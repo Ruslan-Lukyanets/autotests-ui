27ac25051c97735e78d1c2bba6a8921e466c4cd8
Ruslan-Lukyanets <ruslanlukyanets@yandex.ru>
2026-03-07 21:30:12 +0300

    Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,51 +1,35 @@
 import allure
 import pytest
-import os
-from playwright.sync_api import Page, Playwright
-
-from pages.authentication.registration_page import RegistrationPage
 from _pytest.fixtures import SubRequest
+from playwright.sync_api import Playwright, Page
+from tools.playwright.pages import initialize_playwright_page
+from pages.authentication.registration_page import RegistrationPage
+
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
-        yield browser.new_page()
-
-        context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-        browser.close()
-
-        allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
 
-        registration_page = RegistrationPage(page=page)
-        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        registration_page.registration_form.fill(email='user@gmail.com', username='username', password='password')
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        registration_page.click_registration_button()
+    registration_page = RegistrationPage(page=page)
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.click_registration_button()
 
-        parent_dir = os.path.dirname(os.path.abspath(__file__))
-        root_dir = os.path.dirname(parent_dir)
-        storage_state_path = os.path.join(root_dir, "browser-state.json")
-        context.storage_state(path=storage_state_path)
-        browser.close()
+    context.storage_state(path="browser-state.json")
+    browser.close()
+
 
 @pytest.fixture
-def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright):
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json")
-        context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-        yield context.new_page()
-
-        context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-        browser.close()
-
-        allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state="browser-state.json"
+    )
