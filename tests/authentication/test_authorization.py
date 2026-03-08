4b3811a16806597e30db9d0af9211a45880e6dfa
Ruslan-Lukyanets <ruslanlukyanets@yandex.ru>
2026-03-08 18:47:50 +0300

    Параллельный запуск автотестов

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -13,6 +13,7 @@ from tools.allure.tags import AllureTag
 from tools.routes import AppRoute
 
 
+
 @pytest.mark.regression
 @pytest.mark.authorization
 @allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
@@ -23,6 +24,7 @@ from tools.routes import AppRoute
 @allure.suite(AllureFeature.AUTHENTICATION)
 @allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthorization:
+    @pytest.mark.xdist_group(name='authorization-group')
     @pytest.mark.parametrize(
         "email, password",
         [
